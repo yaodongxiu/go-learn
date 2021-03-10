8f4a51a95311e3428845e2d0ff90f33a7db3e04c
yaodongxiu <945726867@qq.com>
2021-03-10 19:25:26 +0800

    update

--- test/7.go
+++ test/7.go
@@ -7,6 +7,12 @@ func main() {
 	for i := 0; i < len(theme); i++ {
 		fmt.Printf("%c %d\n", theme[i], theme[i])
 	}
+
+	theme2 := []byte(theme)
+	for index, s := range theme2 {
+		fmt.Printf("%d %c %d\n", index, s, s)
+	}
+
 	for index, s := range theme {
 		fmt.Printf("%d %c %d\n", index, s, s)
 	}
