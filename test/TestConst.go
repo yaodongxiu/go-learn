b325e217fdc35207e85b46faae5c547f754354d4
yaodongxiu <945726867@qq.com>
2021-04-17 17:40:56 +0800

    update: test time

--- test/TestConst.go
+++ test/TestConst.go
@@ -18,15 +18,18 @@ func main() {
 	fmt.Println(bookingType1)
 	fmt.Println(bookingType2)
 	fmt.Println(bookingType3)
+	aaa := "aaa"
+	aaa = aaa + bookingType2.String()
+	fmt.Println(aaa)
 }
 
 func (bt BookingType) String() string {
 	switch bt {
 	case Finish:
-		return "默认"
-	case Cancel:
 		return "落位完成"
-	default:
+	case Cancel:
 		return "取消落位"
+	default:
+		return "默认"
 	}
 }
