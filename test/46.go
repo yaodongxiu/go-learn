4eeb7fade0c7b1d3ac759323122a876dd55f7bd8
yaodongxiu <945726867@qq.com>
2021-05-14 17:55:36 +0800

    update:

--- test/46.go
+++ test/46.go
@@ -6,6 +6,14 @@ import (
 )
 
 func main() {
+	dd := make(chan int, 1)
+	close(dd) // chan close 后，不再有阻塞的效果，而且无论chan是有缓冲或者没有缓冲的，close后，vv=0， ok=false
+	vv, ok := <-dd
+	fmt.Println(ok)   // false
+	fmt.Println(vv)   // 0
+	fmt.Println(<-dd) // 0
+	fmt.Println(<-dd) // 0
+	fmt.Println(<-dd) // 0
 	cc := make(chan int, 1)
 	i := 0
 	for {
