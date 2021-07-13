150d239ccc4525b14d8e27ee45042cda1d444ecb
yaodongxiu <945726867@qq.com>
2021-07-13 16:53:32 +0800

    update: error-handle

--- error-handle/sample4/main.go
+++ error-handle/sample4/main.go
@@ -12,7 +12,7 @@ import (
 func ReadFile(path string) ([]byte, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, errors.Wrapf(err, "ReadFile(path:%s) failed", path)
+		return nil, errors.Wrapf(err, "os.Open(path:%s) failed", path)
 	}
 	defer f.Close()
 	buf, err := ioutil.ReadAll(f)
@@ -25,14 +25,14 @@ func ReadFile(path string) ([]byte, error) {
 func ReadConfig() ([]byte, error) {
 	home := os.Getenv("HOME")
 	config, err := ReadFile(filepath.Join(home, ".settings.xml"))
-	return config, errors.Wrapf(err, "ReadFile(path:%s) failed", filepath.Join(home, ".settings.xml"))
+	return config, errors.Wrapf(err, "svc.ReadFile(path:%s) failed", filepath.Join(home, ".settings.xml"))
 }
 
 func main() {
 	_, err := ReadConfig()
 	if err != nil {
-		fmt.Printf("%+v\n", errors.Cause(err))
-		fmt.Println(err)
+		fmt.Println(errors.Cause(err))
+		fmt.Println(err.Error())
 		//fmt.Printf("%+v\n", err)
 	}
 }
